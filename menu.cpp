15859933696dc451cb6a03fa800435cafc045233
JimenaDiazFranco <a01403295@tec.mx>
2024-11-26 18:51:48 -0600

    Add files via upload
    Task_5 Menu

---
+++ menu.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "UserManager.h"
+//#include "ContentManager.h"
+//#include "Graph.h"
+//#include "RecommendationSystem"
+using namespace std;
+
+void displayMenu(){
+    int choice;
+    do{
+        cout << "............ MENU ..........."<<endl;
+        cout << "1. Crear amistad" <<endl;
+        cout << "2. Agregar contenido" <<endl;
+        cout << "3. Agregar interes a un usuario" <<endl;
+        cout << "4. Ver recomendaciones" <<endl;
+        cout << "5. Agregar usuario" <<endl;
+        cout << "Salir" <<endl<<endl;
+
+        cout <<"Seleccione una opcion: ";
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                //addFriend();
+                break;
+            case 2:
+                //addContent();
+                break;
+            case 3:
+                //addInterest();
+                break;
+            case 4:
+                //getRecommendations();
+                break;
+            case 5:
+                cout << "Saliendo del sistema..."<<endl;
+                break;
+            case 6:
+                break;
+            default:
+                cout << "Opcion no valida"<<endl;
+                break;
+        }
+    } 
+    while(choice != 5);
+
+    
+
+}
+
+    
+    
